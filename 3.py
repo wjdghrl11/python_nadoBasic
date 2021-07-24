5163b178be6a261fb03f06cb4226b64f0495a40f
wjdghrl11 <wjdghrl26@gmail.com>
2021-07-24 21:50:06 +0800

    python1

---
+++ 3.py
@@ -0,0 +1,8 @@
+# boolean 참과 거짓
+print(5 > 10) 
+print(5 < 10)
+print(True) # 이런식으로도 표현가능
+print(False)
+print(not True) # 반대
+print(not False)
+print(not(5 > 10)) 
---
+++ 4.py
@@ -0,0 +1,10 @@
+# 변수
+animal = "강아지"
+age = 5
+name = "삼식이"
+hobby = "산책"
+is_adult = age >= 5
+
+print("우리집" + animal + "이름은" + name + "이다")
+print(name + "는" + str(age) + "살" + hobby + "좋아함")
+print(name + "어른?" + str(is_adult))
